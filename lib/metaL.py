22146586325b1530c944b7e252d1301a35fe0d6d
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-23 01:50:01 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/metaL.py
+++ lib/metaL.py
@@ -193,7 +193,16 @@ extensions = JSON('extensions.json')
 
 def tasks():
     json = JSON('tasks.json')
-    # json / S(None,'{','}')
+    json / (S(None, '{', '}')
+            / r'"version": "2.0.0",'
+            / (S(None, '"tasks": [', ']')
+               / (S(None, '{', '}')
+                  / r'"label"          : "python: run",'
+                  / r'"type"           : "shell",'
+                  / r'"command"        : "python3 ${file}",'
+                  / r'"problemMatcher" : [],'
+                  / r'"presentation"   : {"showReuseMessage": false, "focus": false, "reveal": "silent"},'
+                  / r'"group"          : {"kind": "build","isDefault": true}')))
     return json
 
 launch = JSON('launch.json')
