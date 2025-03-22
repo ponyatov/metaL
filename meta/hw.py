c6047bd42e5927c7e26883aca00009803f471d0c
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-23 03:03:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/hw.py
+++ meta/hw.py
@@ -3,5 +3,7 @@ from IO import *
 hw = Dir('hw')
 hw / (File('.gitignore') / '!.gitignore')
 
-inc = Dir('inc'); hw / inc
+inc = Dir('inc'); hw / inc; inc / File('hw.hpp')
+src = Dir('src'); hw / src; src / File('hw.cpp')
+
 hw.sync()
