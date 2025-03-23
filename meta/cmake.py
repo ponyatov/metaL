553cd263f446b69125e86362db7e7d65311a51ee
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-23 04:03:19 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/cmake.py
+++ meta/cmake.py
@@ -2,8 +2,63 @@ from IO import *
 
 cmake = Dir('cmake')
 
-x86 = File('x86_64-linux-gnu.cmake');cmake/x86
+x86 = File('x86_64-linux-gnu.cmake'); cmake / x86
+(x86
+ / r'set(CMAKE_SYSTEM_NAME       Linux)'
+ / r'set(CMAKE_SYSTEM_PROCESSOR  x86_64)'
+ / r'set(TOOLCHAIN_PREFIX        ${ARCH}-${OS}-gnu)'
+ / r'set(CMAKE_EXECUTABLE_SUFFIX "")'
+ / ''
+ / r'add_compile_options("-march=native")'
+ / ''
+ / r'include(any_toolchain)'
+ )
 
+any = File('any_toolchain.cmake'); cmake / any
+(any
+ / r'set(CMAKE_C_STANDARD   17)'
+ / r'set(CMAKE_CXX_STANDARD 17)'
+ / ''
+ / r'set(CMAKE_C_COMPILER_FORCED   TRUE)'
+ / r'set(CMAKE_CXX_COMPILER_FORCED TRUE)'
+ / r'set(CMAKE_C_COMPILER_ID       GNU)'
+ / r'set(CMAKE_CXX_COMPILER_ID     GNU)'
+ / ''
+ / r'set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}-gcc)'
+ / r'set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})'
+ / r'set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)'
+ / r'set(CMAKE_LINKER       ${CMAKE_C_COMPILER})'
+ / r'set(CMAKE_OBJCOPY      ${TOOLCHAIN_PREFIX}-objcopy)'
+ / r'set(CMAKE_SIZE         ${TOOLCHAIN_PREFIX}-size)'
+ / r'set(CMAKE_RC_COMPILER  ${TOOLCHAIN_PREFIX}-windres)'
+ / ''
+ / r'string(TOUPPER ${HW}   HW_  )'
+ / r'string(TOUPPER ${CPU}  CPU_ )'
+ / r'string(TOUPPER ${ARCH} ARCH_)'
+ / r'string(TOUPPER ${OS}   OS_  )'
+ / ''
+ / (S(None, 'add_compile_options(', ')')
+    / r'-Wall -Wextra -Wpedantic'
+    / r'$<$<CONFIG:Debug>:-DDEBUG>')
+ / ''
+ / (S(None, 'add_compile_definitions(', ')')
+    / r'${HW_} ${CPU_} ${ARCH_} ${OS_}')
+    / ''
+    / (S(None, 'add_link_options(', ')')
+       / r'-Wl,--print-memory-usage')
+ / ''
+ / r'if(CMAKE_BUILD_TYPE MATCHES Debug)'
+ / r'    add_compile_options(-O0 -g3)'
+ / r'endif()'
+ / r'if(CMAKE_BUILD_TYPE MATCHES Release)'
+ / r'    add_compile_options(-Os -g0)'
+ / r'endif()'
+ / ''
+ / r'set(CMAKE_EXECUTABLE_SUFFIX_ASM ${CMAKE_EXECUTABLE_SUFFIX})'
+ / r'set(CMAKE_EXECUTABLE_SUFFIX_C   ${CMAKE_EXECUTABLE_SUFFIX})'
+ / r'set(CMAKE_EXECUTABLE_SUFFIX_CXX ${CMAKE_EXECUTABLE_SUFFIX})'
+
+ )
 
 lists = File('CMakeLists.txt')
 
@@ -11,6 +66,9 @@ lists = File('CMakeLists.txt')
  / r'cmake_minimum_required(VERSION 3.22)'
  / r'get_filename_component(CMAKE_PROJECT_NAME ${CMAKE_SOURCE_DIR} NAME_WE)'
  / r'project(${CMAKE_PROJECT_NAME} LANGUAGES C CXX ASM)'
+ / ''
+ / r'include(version)'
+ / r'include(src)'
  / ''
  / r'message("-- |")'
  / r'message("-- | toolchain: " ${CMAKE_CXX_COMPILER} " @ " ${CMAKE_TOOLCHAIN_FILE})'
--- meta/presets.py
+++ meta/presets.py
@@ -39,7 +39,7 @@ pc = (H('pc', inherits='common')
       / '"cacheVariables"  : {"HW":"pc", "CPU":"i5", "ARCH":"x86_64"}')
 
 linux = (C('linux', inherits='pc')
-         / r'"toolchainFile"   : "x86_64-linux-gnu.cmake",'
+         / r'"toolchainFile"   : "${sourceDir}/cmake/x86_64-linux-gnu.cmake",'
          / r'"cacheVariables"  : {"OS":"linux"}')
 linux.sfx = '}'
 
